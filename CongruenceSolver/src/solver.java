546da9450466710af45ed51aafd3b7bbacff3dd9
eddy-qiu <eqiu22@gmail.com>
2023-05-14 23:05:07 -0400

    init

---
+++ CongruenceSolver/src/solver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class solver {
+    public static void solve(ArrayList<Integer> vals, ArrayList<Integer> mods) {
+        if(vals != null && mods != null && vals.size() == mods.size()) {
+            
+        }
+    }
+}
